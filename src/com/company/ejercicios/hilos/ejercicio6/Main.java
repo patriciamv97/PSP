4a6c7bc1851d5764bdcaab378412fb4e888dfba4
Patricia <pmaceirasvarela@danielcastelao.org>
2021-11-12 08:53:01 +0100

    ejercicio 6 terminado

--- src/com/company/ejercicios/hilos/ejercicio6/Main.java
+++ src/com/company/ejercicios/hilos/ejercicio6/Main.java
@@ -2,38 +2,36 @@ package com.company.ejercicios.hilos.ejercicio6;
 
 public class Main {
 
-    static int caja=100000;
+    static int caja = 100000;
 
     public static void main(String[] args) {
 
-        new Ingreso("1000").start();
-        new Ingreso("1000").start();
-        new Ingreso("1000").start();
-        new Ingreso("1000").start();
-        new Ingreso("1000").start();
-        new Ingreso("1000").start();
-        new Ingreso("1000").start();
-        new Ingreso("1000").start();
-        new Ingreso("1000").start();
-        new Ingreso("1000").start();
+        for (int i = 0; i < 10; i++) {
+            new Ingreso("1000").start();
+            new Extraccion("1000").start();
 
-       new Extraccion("50").start();
-        new Extraccion("50").start();
-        new Extraccion("50").start();
-        new Extraccion("50").start();
-        new Extraccion("50").start();
+        }
 
+       /*
+
+          for (int i = 0; i < 5; i++) {
+            new Extraccion("1000").start();
+        }
+
+        */
     }
+
     public static class Ingreso extends Thread {
         public Ingreso(String name) {
             super(name);
         }
 
         public void run() {
-          caja += Integer.parseInt(getName());
-            System.out.println("Ingreso realizado con éxito, total en caja : "+caja);
+            caja += Integer.parseInt(getName());
+            System.out.println("Ingreso realizado con éxito, total en caja : " + caja);
         }
     }
+
     public static class Extraccion extends Thread {
         public Extraccion(String name) {
             super(name);
@@ -41,7 +39,7 @@ public class Main {
 
         public void run() {
             caja -= Integer.parseInt(getName());
-            System.out.println("Extracción  realizado con éxito, total en caja : "+caja);
+            System.out.println("Extracción  realizada con éxito, total en caja : " + caja);
         }
     }
 }
